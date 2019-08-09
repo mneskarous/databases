ca5d9a8c7d5b93ed3fb7760cc071bcbe8493d0af
Michael Eskarous <mneskarous@gmail.com>
2019-08-08 22:00:55 -0700

    Completed in separate attempt and transferred to master branch

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,17 +1,44 @@
 var models = require('../models');
 
 module.exports = {
-  messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+  messages: { // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get((err, results) => {
+        if (err) {
+          console.error(err);
+        }
+        res.json(results);
+      });
+    },
+    post: function (req, res) { // a function which handles posting a message to the database
+      var params = [req.body.username, req.body.message, req.body.roomname];
+      models.messages.post(params, (err, results) => {
+        if (err) {
+          console.error(err);
+        }
+        res.sendStatus(201);
+      });
+    }
   },
 
   users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get((err, results) => {
+        if (err) {
+          console.error(err);
+        }
+        res.json(results);
+      });
+    },
+    post: function (req, res) {
+      var params = [req.body.username];
+      models.users.post(params, (err, results) => {
+        if (err) {
+          console.error(err);
+        }
+        res.sendStatus(201);
+      });
+    }
   }
 };
 
-
-// SECOND STEP !!!
