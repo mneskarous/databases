edf8022dff472ab2a0a8d6bd065ef82755d6b6e9
Michael Eskarous <mneskarous@gmail.com>
2019-08-08 22:03:10 -0700

    Completed in separate attempt and transferred to master branch

--- server/models/index.js
+++ server/models/index.js
@@ -2,16 +2,41 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      // fetch all messages
+      // need id, message, roomname, and username
+      var queryString = "select messages.id, messages.message, messages.roomname \
+      from messages left outer join users on (messages.userID = users.id \
+      order by messages.id desc";
+      db.query(queryString, (err, results) => {
+        callback(err, results);
+      });
+    },
+    post: function (params, callback) {
+      // create a message
+      var queryString = "insert into messages(userID, message, roomname) \
+      value (?, (select id from users where username = ? limit 1), ?)";
+      db.query(queryString, params, (err, results) => {
+        callback(err, results);
+      });
+    }
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      // fetch all users
+      var queryString = "select * from users";
+      db.query(queryString, (err, results) => {
+        callback(err, results)
+      });
+    },
+    post: function (params, callback) {
+      // create a user
+      var queryString = "insert into users(username) values (?)";
+      db.query(queryString, params, (err, results) => {
+        callback(err, results);
+      });
+    }
   }
 };
-
-
-// START HERE !!!!!
