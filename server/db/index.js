f392810f9751849bc060b55f07af675812798a8b
Michael Eskarous <mneskarous@gmail.com>
2019-08-10 13:56:03 -0700

    Completed in separate attempt and transferred to master branch

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,40 @@
+var Sequalize = requre('Sequelize');
+var orm = new Sequalize('chat', 'root', '');
 var mysql = require('mysql');
+
+var User = orm.define('User', {
+  username: Sequalize.STRING
+});
+
+var Message = orm.define('Message', {
+  userid: Sequalize.INTEGER,
+  message: Sequalize.STRING,
+  roomname: Sequalize.String
+});
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+User.sync();
+Message.sync();
+
+module.exports(User) = User;
+module.exports(Message) = Message;
 
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-var connect = mysql.createConnection({
-  user: "student",
-  password: "student",
-  database: "chat"
-});
+// *** Before ORM Refactor ***
 
-connect.connect();
+// var mysql = require('mysql');
 
-module.exports = connect;
+// var connect = mysql.createConnection({
+//   user: "root",
+//   password: "",
+//   database: "chat"
+// });
+
+// connect.connect();
+
+// module.exports = connect;
